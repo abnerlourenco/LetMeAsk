199f5e9bc3ce1613e22cc8baf0282038875c8377
abnerlourenco <abner2010_hm@hotmail.com>
2021-06-28 14:17:40 -0300

    created home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,34 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
 export function Home() {
   return(
     <div>
       <aside>
-        
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo.</strong>
+        <p>Compartilhe seu conhecimento tirando as dúvidas da sua audiência.</p>
       </aside>
+      <main>
+        <div>
+          <img src={logoImg} alt="LetMeAsk" />
+          <button>
+            <img src={googleIconImg} alt="Logo do google" />
+            Crie sua sala com o Google
+          </button>
+          <div>ou entre em uma sala</div>
+          <form>
+            <input 
+              type="text"
+              placeholder="Digite o código da sala" 
+            />
+            <button type="submit">
+              Entrar na sala
+            </button>
+          </form>
+        </div>
+      </main>
     </div>
   )
 }
